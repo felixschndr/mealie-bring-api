46b78ca71589cc46bcee406cff3010a2af5fccd6
Felix Schneider <fs@felix-schneider.org>
2024-05-29 21:38:19 +0200

    Remove debug logmessages

--- source/ingredient.py
+++ source/ingredient.py
@@ -2,7 +2,7 @@ from errors import IgnoredIngredient
 from logger_mixin import LoggerMixin
 
 
-class Ingredient (LoggerMixin):
+class Ingredient(LoggerMixin):
     def __init__(self, ingredient_input: dict, ignored_ingredients: list[str]):
         super().__init__()
 
@@ -36,20 +36,17 @@ class Ingredient (LoggerMixin):
 
     def _parse_input_with_ingredient_amounts(self) -> None:
         self.log.debug("Parsing input with ingredient amount")
-        self.log.debug(f"Complete ingredient: {self.ingredient_input}")
 
         food_name = self.ingredient_input["food"]["name"]
         if food_name.lower() in self.ignored_ingredients:
             raise IgnoredIngredient(f"Found ignored ingredient {food_name}")
         self.food = food_name
 
-
         quantity = self.ingredient_input.get("quantity", None)
         if quantity:
             self.specification += f"{str(quantity)} "
 
         unit = self.ingredient_input.get("unit", None)
-        self.log.debug(f"Unit: {unit}")
         if unit:
             abbreviation = unit["abbreviation"]
             name = unit["name"]
@@ -57,8 +54,6 @@ class Ingredient (LoggerMixin):
                 self.specification += abbreviation
             elif name:
                 self.specification += name
-
-        self.log.debug(f"Specification afterwards: {self.specification}")
 
         note = self.ingredient_input.get("note", None)
         if note:
