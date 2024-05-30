387d162e2e990c582b6db9b031904800a5527ae9
Felix Schneider <fs@felix-schneider.org>
2024-05-30 14:12:50 +0200

    Rework ingredient parsing

--- source/ingredient.py
+++ source/ingredient.py
@@ -3,11 +3,18 @@ from logger_mixin import LoggerMixin
 
 
 class Ingredient(LoggerMixin):
-    def __init__(self, ingredient_input: dict, ignored_ingredients: list[str]):
+    def __init__(
+        self,
+        ingredient_input: dict,
+        ignored_ingredients: list[str],
+        enable_amount: bool,
+    ):
         super().__init__()
 
         self.ingredient_input = ingredient_input
         self.ignored_ingredients = ignored_ingredients
+
+        self.enable_amount = enable_amount
 
         self.food = None
         self.specification = ""
@@ -20,41 +27,64 @@ class Ingredient(LoggerMixin):
         return self.food
 
     def parse_input(self) -> None:
+        self.log.debug(f"Parsing {self.ingredient_input}")
         try:
-            _ = self.ingredient_input["food"]
-        except KeyError:
+            if self.enable_amount:
+                self._parse_input_with_ingredient_amounts()
+            else:
+                self._parse_input_with_no_ingredient_amounts()
+        except ValueError:
             # Happens if there is an empty ingredient (i.e. added one ingredient but did not fill it out)
             raise ValueError("There is an ingredient with no name, it will be ignored!")
-        if self.ingredient_input["disableAmount"]:
-            self._parse_input_with_no_ingredient_amounts()
-        else:
-            self._parse_input_with_ingredient_amounts()
+
+        self.log.debug(f"Parsed ingredient: {self}")
 
     def _parse_input_with_no_ingredient_amounts(self) -> None:
-        self.log.debug("Parsing input with no ingredient amount")
-        self.food = self.ingredient_input["display"]
+        note = self.ingredient_input["note"]
+        if not note:
+            raise ValueError()
+        self.food = note
 
     def _parse_input_with_ingredient_amounts(self) -> None:
-        self.log.debug("Parsing input with ingredient amount")
+        if not self.ingredient_input["food"]:
+            raise ValueError()
 
         food_name = self.ingredient_input["food"]["name"]
+        food_plural_name = self.ingredient_input["food"]["pluralName"]
+        quantity = int(self.ingredient_input["quantity"] or 0)
+        unit = self.ingredient_input["unit"]
+        note = self.ingredient_input["note"]
+
+        # Ignored check #
         if food_name.lower() in self.ignored_ingredients:
             raise IgnoredIngredient(f"Found ignored ingredient {food_name}")
-        self.food = food_name
 
-        quantity = self.ingredient_input.get("quantity", None)
+        # Food #
+        if quantity and quantity > 1 and food_plural_name:
+            self.food = food_plural_name
+        else:
+            self.food = food_name
+
+        # Quantity #
         if quantity:
-            self.specification += f"{str(quantity)} "
+            self.specification += str(quantity)
 
-        unit = self.ingredient_input.get("unit", None)
+        # Seperator between Quantity and Unit #
+        if quantity and unit:
+            self.specification += " "
+
+        # Unit #
         if unit:
-            abbreviation = unit["abbreviation"]
-            name = unit["name"]
-            if abbreviation:
-                self.specification += abbreviation
-            elif name:
-                self.specification += name
+            unit_name = unit["name"]
+            unit_abbreviation = unit["abbreviation"]
+            unit_plural_name = unit["pluralName"]
+            if unit_abbreviation:
+                self.specification += unit_abbreviation
+            elif unit_plural_name and quantity and quantity > 1:
+                self.specification += unit_plural_name
+            elif unit_name:
+                self.specification += unit_name
 
-        note = self.ingredient_input.get("note", None)
+        # Note #
         if note:
             self.specification += f" ({note})"
