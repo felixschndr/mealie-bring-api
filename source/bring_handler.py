3a80d366a9e11b1309b5419bd4bff9eb38efc194
Felix Schneider <mail@fschneider.me>
2025-07-07 22:11:02 +0200

    Refactor BringHandler to handle token expiration

--- source/bring_handler.py
+++ source/bring_handler.py
@@ -3,6 +3,7 @@ import sys
 
 import aiohttp
 from bring_api import Bring, BringItemOperation, BringNotificationType
+from bring_api.exceptions import BringAuthException
 from environment_variable_getter import EnvironmentVariableGetter
 from ingredient import Ingredient
 from logger_mixin import LoggerMixin
@@ -12,22 +13,22 @@ class BringHandler(LoggerMixin):
     def __init__(self, loop: asyncio.AbstractEventLoop):
         super().__init__()
 
-        self.bring = loop.run_until_complete(self.initialize())
+        self.bring = None
         self.list_uuid = loop.run_until_complete(self.determine_list_uuid())
 
-    async def initialize(self) -> Bring:
-        username = EnvironmentVariableGetter.get("BRING_USERNAME")
-        password = EnvironmentVariableGetter.get("BRING_PASSWORD")
-
-        session = aiohttp.ClientSession()
-        bring = Bring(session, username, password)
+    async def _login(self) -> None:
+        self.bring = Bring(
+            aiohttp.ClientSession(),
+            EnvironmentVariableGetter.get("BRING_USERNAME"),
+            EnvironmentVariableGetter.get("BRING_PASSWORD"),
+        )
         self.log.info("Attempting the login into Bring")
-        await bring.login()
+        await self.bring.login()
         self.log.info("Login successful")
 
-        return bring
-
     async def determine_list_uuid(self) -> str:
+        await self._login()
+
         list_name = EnvironmentVariableGetter.get("BRING_LIST_NAME")
 
         list_name_lower = list_name.lower()
@@ -41,8 +42,20 @@ class BringHandler(LoggerMixin):
         sys.exit(1)
 
     async def add_items(self, ingredients: list[Ingredient]) -> None:
-        items = [ingredient.to_dict() for ingredient in ingredients]
-        await self.bring.batch_update_list(self.list_uuid, items, BringItemOperation.ADD)
+        try:
+            await self._add_items(ingredients)
+        except BringAuthException as e:
+            if "expired" not in str(e):
+                raise e
+
+            self.log.info("The authentication token has expired. Re-logging in...")
+            await self._login()
+            await self._add_items(ingredients)
+
+    async def _add_items(self, ingredients: list[Ingredient]) -> None:
+        await self.bring.batch_update_list(
+            self.list_uuid, [ingredient.to_dict() for ingredient in ingredients], BringItemOperation.ADD
+        )
 
     async def notify_users_about_changes_in_list(self) -> None:
         self.log.debug("Notifying users about changes in shopping list")
